075874edb2e65d03ee8d66b5a27427a62dd85f84
Luke Jones <luke@ljones.dev>
2020-01-01 21:29:43 +1300

    Benchmarks and pipelines

--- wad/benches/bench.rs
+++ wad/benches/bench.rs
@@ -1,7 +1,7 @@
 use criterion::*;
 use wad::WadFile;
 
-fn bench_iter_safe(b: &mut Bencher, _i: &u32) {
+fn bench_doom1(b: &mut Bencher, _i: &u32) {
     b.iter(|| {
         let mut wad = WadFile::new("../doom1.wad");
         wad.load();
@@ -9,10 +9,29 @@ fn bench_iter_safe(b: &mut Bencher, _i: &u32) {
     });
 }
 
+fn bench_doom(b: &mut Bencher, _i: &u32) {
+    b.iter(|| {
+        let mut wad = WadFile::new("../doom.wad");
+        wad.load();
+        wad.read_directories();
+    });
+}
+
+fn bench_doom2(b: &mut Bencher, _i: &u32) {
+    b.iter(|| {
+        let mut wad = WadFile::new("../doom2.wad");
+        wad.load();
+        wad.read_directories();
+    });
+}
+
 fn bench(c: &mut Criterion) {
-    let load_and_read = Fun::new("Load and read shareware wad", bench_iter_safe);
-    let fun = vec![load_and_read];
-    c.bench_functions("Iteration and modify", fun, 10000);
+    let fun = vec![
+        Fun::new("Load and read shareware wad", bench_doom1),
+        Fun::new("Load and read ultimate wad", bench_doom),
+        Fun::new("Load and read Doom II wad", bench_doom2),
+    ];
+    c.bench_functions("WAD Loading", fun, 100);
 }
 
 criterion_group!(benches, bench,);
