ea3398cfd053c4c92c6605d812f241d89d732c64
Luke D. Jones <luke@ljones.dev>
2022-05-10 11:29:42 +1200

    Make wipe more visible

---
+++ intermission/doom/src/defs.rs
@@ -0,0 +1,35 @@
+pub(crate) const MAP_POINTS: [[(u32, u32); 9]; 3] = [
+    [
+        (185, 164), // location of level 0 (CJ)
+        (148, 143), // location of level 1 (CJ)
+        (69, 122),  // location of level 2 (CJ)
+        (209, 102), // location of level 3 (CJ)
+        (116, 89),  // location of level 4 (CJ)
+        (166, 55),  // location of level 5 (CJ)
+        (71, 56),   // location of level 6 (CJ)
+        (135, 29),  // location of level 7 (CJ)
+        (71, 24),   // location of level 8 (CJ)
+    ],
+    [
+        (254, 25),  // location of level 0 (CJ)
+        (97, 50),   // location of level 1 (CJ)
+        (188, 64),  // location of level 2 (CJ)
+        (128, 78),  // location of level 3 (CJ)
+        (214, 92),  // location of level 4 (CJ)
+        (133, 130), // location of level 5 (CJ)
+        (208, 136), // location of level 6 (CJ)
+        (148, 140), // location of level 7 (CJ)
+        (235, 158), // location of level 8 (CJ)
+    ],
+    [
+        (156, 168), // location of level 0 (CJ)
+        (48, 154),  // location of level 1 (CJ)
+        (174, 95),  // location of level 2 (CJ)
+        (265, 75),  // location of level 3 (CJ)
+        (130, 48),  // location of level 4 (CJ)
+        (279, 23),  // location of level 5 (CJ)
+        (198, 48),  // location of level 6 (CJ)
+        (140, 25),  // location of level 7 (CJ)
+        (281, 136), // location of level 8 (CJ)
+    ],
+];
