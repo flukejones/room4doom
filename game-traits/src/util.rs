4c92c79936158ab274708f99f83dd698417317d4
Luke D. Jones <luke@ljones.dev>
2022-05-12 13:42:01 +1200

    Show keys in statusbar

--- game-traits/src/util.rs
+++ game-traits/src/util.rs
@@ -4,7 +4,7 @@ use std::mem::MaybeUninit;
 use wad::{lumps::WadPatch, WadData};
 
 /// Pattern like `WINUM` or `STTNUM`
-pub fn get_num_sprites(pattern: &str, mut start: usize, wad: &WadData) -> [WadPatch; 10] {
+pub fn get_num_sprites(pattern: &str, start: usize, wad: &WadData) -> [WadPatch; 10] {
     let mut nums: [MaybeUninit<WadPatch>; 10] = [
         MaybeUninit::uninit(),
         MaybeUninit::uninit(),
@@ -20,6 +20,22 @@ pub fn get_num_sprites(pattern: &str, mut start: usize, wad: &WadData) -> [WadPa
     for n in 0..10 {
         let p = n + start;
         let lump = wad.get_lump(&format!("{pattern}{p}")).unwrap();
+        nums[n] = MaybeUninit::new(WadPatch::from_lump(lump));
+    }
+    unsafe { nums.map(|n| n.assume_init()) }
+}
+
+pub fn get_st_key_sprites(wad: &WadData) -> [WadPatch; 6] {
+    let mut nums: [MaybeUninit<WadPatch>; 6] = [
+        MaybeUninit::uninit(),
+        MaybeUninit::uninit(),
+        MaybeUninit::uninit(),
+        MaybeUninit::uninit(),
+        MaybeUninit::uninit(),
+        MaybeUninit::uninit(),
+    ];
+    for n in 0..6 {
+        let lump = wad.get_lump(&format!("STKEYS{n}")).unwrap();
         nums[n] = MaybeUninit::new(WadPatch::from_lump(lump));
     }
     unsafe { nums.map(|n| n.assume_init()) }
