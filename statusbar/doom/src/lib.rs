4dcd5430f9b32f8b7c4d43a26f2c94856d8df6fb
Luke D. Jones <luke@ljones.dev>
2024-07-01 21:57:57 +1200

    Fmt

--- statusbar/doom/src/lib.rs
+++ statusbar/doom/src/lib.rs
@@ -6,7 +6,8 @@
 use faces::DoomguyFace;
 use gamestate_traits::util::{draw_num_pixels, get_num_sprites, get_st_key_sprites};
 use gamestate_traits::{
-    AmmoType, GameMode, GameTraits, MachinationTrait, PixelBuffer, PlayerStatus, Scancode, WeaponType, WEAPON_INFO
+    AmmoType, GameMode, GameTraits, MachinationTrait, PixelBuffer, PlayerStatus, Scancode,
+    WeaponType, WEAPON_INFO,
 };
 use std::collections::HashMap;
 use wad::types::{WadPalette, WadPatch};
